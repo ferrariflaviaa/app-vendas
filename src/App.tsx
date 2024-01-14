56abd2b78257bb85af3adf5fa037018bdd950f64
ferrariflaviaa <flaviaferrarit@gmail.com>
2024-01-14 14:28:10 -0300

    feat: optimization of settings in the App file

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { executeCreateSqliteTables } from './database';
+import { NavigationContainer } from "@react-navigation/native";
+import { ThemeProvider } from 'styled-components';
+import theme from './themes';
+
+export default function App() {
+  
+  useEffect(() => {
+    executeCreateSqliteTables();
+  });
+
+  return (
+    <NavigationContainer>
+      <ThemeProvider theme={theme}>
+          <StatusBar backgroundColor={theme.COLORS.PRIMARY} />
+        </ThemeProvider>
+    </NavigationContainer>
+  );
+}
+
