4b37e7a3963d7e7e7e4b4dae943327f9a4ef13b9
ferrariflaviaa <flaviaferrarit@gmail.com>
2024-01-14 12:59:25 -0300

    feat: start project

---
+++ src/themes/index.ts
@@ -0,0 +1,17 @@
+export default {
+  COLORS: {
+    WHITE: "#FFFFFF",
+    PRIMARY: "#037782",
+    SECONDARY: "#52aab2",
+    // PRIMARY: '#18A748',
+    // SECONDARY: '#47BA6E',
+    GREY: "#454545",
+    GREY10: "#EEEEEE",
+    GREY50: "#BAB8B8",
+    BLACK: "#000000",
+    INPUT: "#EEEEEE",
+    RED: "#CA2020",
+    YELLOW: "#E9E120",
+    ORANGE: "#DF8736",
+  },
+};
